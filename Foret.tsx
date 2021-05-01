9b33ba3656e946fa39ce839cb8f9b5c091cf4216
Jeromeprio <jeromepriolet@gmail.com>
2021-05-01 10:27:10 +0200

    MAJ map

--- Foret.tsx
+++ Foret.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Foret" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+<tileset version="1.5" tiledversion="1.5.0" name="forest_tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="TILES/LPC_forest/LPC_forest/forest_tiles.png" width="512" height="512"/>
 </tileset>
