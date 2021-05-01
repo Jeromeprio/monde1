924f5e991f49bb7817773cd9b2e5edfe8e099c23
Jeromeprio <jeromepriolet@gmail.com>
2021-05-01 19:07:19 +0200

    resummit

---
+++ 5.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="5" tilewidth="32" tileheight="32" tilecount="90" columns="9">
+ <image source="TILES/Game assests/tilesets/5.png" width="288" height="320"/>
+ <tile id="66">
+  <animation>
+   <frame tileid="72" duration="1000"/>
+   <frame tileid="75" duration="1000"/>
+   <frame tileid="78" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <animation>
+   <frame tileid="64" duration="1000"/>
+   <frame tileid="67" duration="1000"/>
+   <frame tileid="70" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="65" duration="1000"/>
+   <frame tileid="68" duration="1000"/>
+   <frame tileid="71" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="75">
+  <animation>
+   <frame tileid="72" duration="1000"/>
+   <frame tileid="75" duration="1000"/>
+   <frame tileid="78" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="76">
+  <animation>
+   <frame tileid="73" duration="1000"/>
+   <frame tileid="76" duration="1000"/>
+   <frame tileid="79" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="77">
+  <animation>
+   <frame tileid="74" duration="1000"/>
+   <frame tileid="77" duration="1000"/>
+   <frame tileid="80" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="500"/>
+   <frame tileid="81" duration="500"/>
+   <frame tileid="87" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="85">
+  <animation>
+   <frame tileid="82" duration="500"/>
+   <frame tileid="85" duration="500"/>
+   <frame tileid="88" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="86">
+  <animation>
+   <frame tileid="83" duration="1000"/>
+   <frame tileid="86" duration="1000"/>
+   <frame tileid="89" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ sol vert.tsx
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="7" tilewidth="32" tileheight="32" tilecount="102" columns="17">
+ <image source="TILES/Game assests/tilesets/7.png" width="560" height="192"/>
+ <wangsets>
+  <wangset name="herbe" type="corner" tile="35">
+   <wangcolor name="herbe" color="#ff0000" tile="35" probability="1"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="13" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="21" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="30" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="31" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="35" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="51" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="52" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="53" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="55" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="56" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="60" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="62" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="64" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="65" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="72" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="73" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="79" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="81" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="96" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="98" wangid="0,0,0,0,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
