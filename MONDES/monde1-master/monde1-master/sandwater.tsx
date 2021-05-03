3bd7727d09b1ee3f4d628f53cd64eb59187158d0
Jeromeprio <jeromepriolet@gmail.com>
2021-05-03 22:09:50 +0200

    update

---
+++ MONDES/monde1-master/monde1-master/sandwater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="sandwater" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="TILES/lpc_terrain/twosided/sandwater.png" width="160" height="192"/>
+</tileset>
